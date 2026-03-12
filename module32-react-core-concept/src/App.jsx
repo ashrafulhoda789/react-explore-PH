f702f467217202ca1f6fe735f9b05209ad81c36b
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-13 02:15:08 +0600

    event handling in react

--- module32-react-core-concept/src/App.jsx
+++ module32-react-core-concept/src/App.jsx
@@ -5,23 +5,39 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    alert("I am clicked");
+  }
+
+  const handleClick3 = ()=>{
+    alert("Clicked 3");
+  }
+
+  const handleAdd = (num)=>{
+    const newNum = num + 5;
+    alert(newNum);
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        
-      </section>
-
       
+      <h3>Vite + React</h3>
+      <button onClick={handleClick}>Click Me</button>
 
-      
+      <button onClick={
+        function handleClick2(){
+          alert("Clicked 2");
+        }
+      }>Click Me2</button>
 
+      <button onClick={handleClick3}>Click me 3</button>
+
+      <button onClick={()=>{
+        alert("Click 4")
+      }}>Click me 4</button>
+
+      <button onClick={()=> handleAdd(5)}>Click 5</button>
       
     </>
   )
