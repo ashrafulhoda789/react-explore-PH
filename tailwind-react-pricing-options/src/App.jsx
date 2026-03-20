4d93133f329013e57ef6639678ddcb1de25f7c06
Muktade Mumir <muktadetabassommumir@gmail.com>
2026-03-20 16:34:54 +0600

    import tailwind, daisyUI, google font

---
+++ tailwind-react-pricing-options/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <header>
+        <h1 className='text-7xl underline'>Vite + React</h1>
+      </header>
+
+      
+      
+    </>
+  )
+}
+
+export default App
