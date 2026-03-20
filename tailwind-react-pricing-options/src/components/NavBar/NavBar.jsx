135dcdd58eb96cbf5b6d6fb3f157fbe808938036
asrafulhoda789 <asrafulhoda789@gmail.com>
2026-03-20 20:24:22 +0600

    responsive navbar desined

--- tailwind-react-pricing-options/src/components/NavBar/NavBar.jsx
+++ tailwind-react-pricing-options/src/components/NavBar/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
     const links = navData.map(route => <Link key={route.id} route={route}></Link>)
 
     return (
-        <nav className='flex justify-between mx-10'>
+        <nav className='flex justify-between mx-10 mt-4'>
 
             <span className='flex' onClick={() => setOpen(!open)}>
                 {
@@ -45,7 +45,9 @@ const NavBar = () => {
                         <X className='md:hidden'></X> :
                         <Menu className='md:hidden'></Menu>}
                 
-                <ul className='md:hidden'>
+                <ul className={`md:hidden absolute duration-1000
+                     ${open ? 'top-8' : '-top-40'}
+                      bg-white text-black`}>
                     {
                         links
                     }
