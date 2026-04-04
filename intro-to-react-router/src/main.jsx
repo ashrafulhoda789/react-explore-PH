672f5bd19277be16d5437e801c9864a7278b3a1f
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-04 23:23:07 +0600

    react routing

---
+++ intro-to-react-router/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div>Hello from react router</div>
+  },
+  {
+    path : 'about',
+    element : <div>About Section</div>
+  },
+  {
+    path : 'app',
+    Component : App
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
