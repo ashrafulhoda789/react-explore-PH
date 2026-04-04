b95115948d1b1a0d61cad35263bf527f5f3d5d4e
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-04 23:32:22 +0600

    how to setop react router and how to use router

--- intro-to-react-router/src/main.jsx
+++ intro-to-react-router/src/main.jsx
@@ -4,11 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './components/root/Root.jsx';
+import Home from './components/home/Home.jsx';
+import Mobiles from './components/mobiles/Mobiles.jsx';
+import Laptops from './components/laptops/Laptops.jsx';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <div>Hello from react router</div>
+    Component : Root,
+    children : [
+      {index : true, Component : Home},
+      {path : 'mobiles', Component : Mobiles},
+      {path : 'laptops', Component : Laptops}
+    ]
   },
   {
     path : 'about',
