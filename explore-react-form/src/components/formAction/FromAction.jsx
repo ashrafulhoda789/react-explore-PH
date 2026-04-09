1ac8ca2e1ffc31434f468b438cfbaff7d1ec2f79
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-09 21:23:27 +0600

    learn how to get value from input field

---
+++ explore-react-form/src/components/formAction/FromAction.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FromAction = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FromAction;
--- explore-react-form/src/components/simpleForm/SimpleForm.jsx
+++ explore-react-form/src/components/simpleForm/SimpleForm.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const SimpleForm = () => {
+
+    const handleSubmit = (event) =>{
+        event.preventDefault(); // Form server side e default vabe reload hoye jai. ota off korar jonno preventDefault use kora hoyece
+        console.log(event.target.name.value);
+        console.log(event.target.email.value);
+    }
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Your name' />
+                <br />
+                <input type="email" name='email' placeholder='Your email'/>
+                <br />
+                <input type="submit" value="submit" />
+            </form>
         </div>
     );
 };
