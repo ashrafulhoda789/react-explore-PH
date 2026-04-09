9136eabc302ca44a588430679e19698dea195388
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-09 16:19:42 +0600

    explore react form concept

---
+++ explore-react-form/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SimpleForm from './components/simpleForm/SimpleForm'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Explore React Form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
